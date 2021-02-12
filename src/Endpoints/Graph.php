2b4a2b708089f53bb723b316a6daaa69ffad4dd4
Alex Renoki <rennokki@gmail.com>
2021-02-12 13:18:59 +0200

    Updated graph

--- src/Endpoints/Graph.php
+++ src/Endpoints/Graph.php
@@ -16,26 +16,49 @@ class Graph extends Client
 
     public function getAllTimePrices(string $id, string $format = null)
     {
-        $response = $this->query([
-            'compress' => true,
-            'id' => $id,
-        ])->get('/exchange/history/osrs/all')->json();
+        $response = $this->query(['id' => $id, 'compress' => 'true'])
+            ->get('/exchange/history/osrs/all')
+            ->json();
 
-        return collect($response[$id])
-            ->when($format, function ($collection, $format) {
-                return $collection->map(function ($point) use ($format) {
-                    return array_merge($point, [
-                        'timestamp' => Carbon::createFromTimestamp($point['timestamp'])->format($format),
-                    ]);
-                });
-            })
-            ->map(function ($point) {
-                unset($point['id']);
+        return $this->processGraph($response, $id, $format);
+    }
 
-                return $point;
-            })
-            ->sortBy('timestamp')
-            ->values()
-            ->toArray();
+    public function getLast90DaysGraph(string $id, string $format = null)
+    {
+        $response = $this->query(['id' => $id, 'compress' => 'true'])
+            ->get('/exchange/history/osrs/last90d')
+            ->json();
+
+        return $this->processGraph($response, $id, $format);
+    }
+
+    public function getSamplePricesGraph(string $id, string $format = null)
+    {
+        $response = $this->query(['id' => $id, 'compress' => 'true'])
+            ->get('/exchange/history/osrs/sample')
+            ->json();
+
+        return $this->processGraph($response, $id, $format);
+    }
+
+    protected function processGraph(array $response, string $id, string $format = null)
+    {
+        return collect($response[$id])->map(function ($point) use ($format) {
+            $volume = null;
+
+            if (count($point) === 2) {
+                [$timestamp, $price] = $point;
+            } elseif (count($point) === 3) {
+                [$timestamp, $price, $volume] = $point;
+            }
+
+            return [
+                'timestamp' => $format
+                    ? Carbon::createFromTimestamp($timestamp)->format($format)
+                    : $timestamp,
+                'price' => $price,
+                'volume' => $volume,
+            ];
+        })->sortBy('timestamp')->values()->toArray();
     }
 }
--- tests/GraphTest.php
+++ tests/GraphTest.php
@@ -8,9 +8,29 @@ class GraphTest extends TestCase
 {
     public function test_get_all_time_graph()
     {
-        $graph = Runescape::graph()->getAllTimePrices(1161, 'Y-m-d H:i:s');
+        $this->makeAssertionsOnGraph(
+            Runescape::graph()->getAllTimePrices(1161, 'Y-m-d H:i:s')
+        );
+    }
 
+    public function test_get_90d_graph()
+    {
+        $this->makeAssertionsOnGraph(
+            Runescape::graph()->getLast90DaysGraph(1161, 'Y-m-d H:i:s')
+        );
+    }
+
+    public function test_get_sample_graph()
+    {
+        $this->makeAssertionsOnGraph(
+            Runescape::graph()->getSamplePricesGraph(1161, 'Y-m-d H:i:s')
+        );
+    }
+
+    protected function makeAssertionsOnGraph(array $graph)
+    {
         foreach ($graph as $point) {
+            $this->assertTrue(is_string($point['timestamp']));
             $this->assertTrue(is_int($point['price']));
         }
     }
