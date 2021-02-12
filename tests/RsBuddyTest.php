76b1130dee898e578f82a9b37c17b3463fa4f158
Alex Renoki <rennokki@gmail.com>
2021-02-12 23:40:30 +0200

    fixed inclusions

--- tests/RsBuddyTest.php
+++ tests/RsBuddyTest.php
@@ -8,7 +8,7 @@ class RsBuddyTest extends TestCase
 {
     public function test_weekly_graphs()
     {
-        $graphs = Runescape::rsbuddy()->getWeeklyGraph(1511);
+        $graphs = Runescape::rsbuddy()->getWeeklyGraph(1511, 'Y-m-d H:i:s');
 
         foreach ($graphs as $graph) {
             $this->assertNotNull($graph['ts']);
@@ -17,7 +17,7 @@ class RsBuddyTest extends TestCase
 
     public function test_monthly_graphs()
     {
-        $graphs = Runescape::rsbuddy()->getMonthlyGraph(1511);
+        $graphs = Runescape::rsbuddy()->getMonthlyGraph(1511, 'Y-m-d H:i:s');
 
         foreach ($graphs as $graph) {
             $this->assertNotNull($graph['ts']);
